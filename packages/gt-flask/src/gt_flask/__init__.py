03b962e1f6fb8742b89a3819bf3b2718e512912e
Ernest McCarter <ernest@generaltranslation.com>
2026-03-07 14:55:48 -0800

    chore: lint

--- packages/gt-flask/src/gt_flask/__init__.py
+++ packages/gt-flask/src/gt_flask/__init__.py
@@ -5,12 +5,12 @@ from gt_i18n import declare_static, declare_var, decode_vars, get_default_locale
 from gt_flask._setup import initialize_gt
 
 __all__ = [
-  "initialize_gt",
-  "t",
-  "declare_var",
-  "declare_static",
-  "decode_vars",
-  "get_locale",
-  "get_locales",
-  "get_default_locale",
+    "initialize_gt",
+    "t",
+    "declare_var",
+    "declare_static",
+    "decode_vars",
+    "get_locale",
+    "get_locales",
+    "get_default_locale",
 ]
--- packages/gt-i18n/src/gt_i18n/helpers/_locales.py
+++ packages/gt-i18n/src/gt_i18n/helpers/_locales.py
@@ -6,10 +6,12 @@ def get_locale() -> str:
     manager = get_i18n_manager()
     return manager.get_locale()
 
+
 def get_locales() -> list[str]:
     """Get the locales from the I18nManager."""
     manager = get_i18n_manager()
     return manager.get_locales()
+
 
 def get_default_locale() -> str:
     """Get the default locale from the I18nManager."""
